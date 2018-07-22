b0052b727ffbdb3db5aac98826f442205658bad8
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-07-21 22:31:01 -0600

    Make it kinda work

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,43 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
-use serde_derive::Serialize;
 #[macro_use]
-extern crate diesel_derives;
+extern crate diesel;
+#[macro_use]
+extern crate quick_error;
+
+use serde_derive::Serialize;
+use diesel::deserialize::Queryable;
+use diesel::{ExpressionMethods, RunQueryDsl, QueryDsl};
+
+mod schema;
+
+quick_error! {
+    #[derive(Debug)]
+    enum Error {
+        PoolError(e: diesel::r2d2::PoolError) { from() }
+        DBError(e: diesel::result::Error) { from() }
+        NotFound(e: String)
+        Internal(e: String)
+    }
+}
+
+impl<'r> rocket::response::Responder<'r> for Error {
+    fn respond_to(self, _: &rocket::Request) -> rocket::response::Result<'r> {
+        match self {
+            Error::NotFound(e) => {
+                rocket::Response::build()
+                    .status(rocket::http::Status::NotFound)
+                    .header(rocket::http::ContentType::Plain)
+                    .sized_body(std::io::Cursor::new(e))
+                    .ok()
+            },
+            _ => {
+                Err(rocket::http::Status::InternalServerError)
+            }
+        }
+    }
+}
 
 #[derive(Queryable, Serialize)]
 struct Tree {
@@ -12,12 +46,22 @@ struct Tree {
 }
 
 #[get("/trees/<id>")]
-fn trees_get(id: i32, state: rocket::State<ServerState>) -> rocket_contrib::Json<Tree> {
-
+fn trees_get(id: i32, state: rocket::State<ServerState>) -> Result<rocket_contrib::Json<Tree>, Error> {
+    let conn = state.conn.get()?;
+    let res = schema::tree::dsl::tree.filter(schema::tree::dsl::id.eq(id)).load::<Tree>(&conn)?;
+    if res.len() < 1 {
+        Err(Error::NotFound("No such tree".to_owned()))
+    }
+    else if res.len() > 1 {
+        Err(Error::Internal("More than one tree returned for single ID".to_owned()))
+    }
+    else {
+        Ok(rocket_contrib::Json(res.into_iter().next().unwrap()))
+    }
 }
 
 struct ServerState {
-    conn: diesel::r2d2::Pool<diesel::r2d2::ConnectionManager>
+    conn: diesel::r2d2::Pool<diesel::r2d2::ConnectionManager<diesel::pg::PgConnection>>
 }
 
 fn main() {
